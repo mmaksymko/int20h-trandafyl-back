7ff1af39154185574fb29b5b7745766adc708e2d
maksymkomyna@gmail.com <maksymkomyna@gmail.com>
2024-03-02 16:02:34 +0200

    [feat] first commit

---
+++ src/main/java/dev/trandafyl/int20htrandafylback/Int20hTrandafylBackApplication.java
@@ -0,0 +1,13 @@
+package dev.trandafyl.int20htrandafylback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Int20hTrandafylBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Int20hTrandafylBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/trandafyl/int20htrandafylback/Int20hTrandafylBackApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.trandafyl.int20htrandafylback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Int20hTrandafylBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
